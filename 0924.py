fee3e7e81b9a472726004bd6669bc54faf6a9236
GUNHEE0316 <p0316gh@gmail.com>
2025-09-24 17:55:11 +0900

    0924 백준풀이

---
+++ 0924.py
@@ -0,0 +1,35 @@
+#4-1
+#import sys
+# N = int(sys.stdin.readline())
+# b_list= list(map(int,sys.stdin.readline().split()))
+# v =int(sys.stdin.readline())
+# print(b_list.count(v))
+
+#4-2
+# import sys
+
+# N,X=map(int,sys.stdin.readline().split())
+
+# my_list=list(map(int,sys.stdin.readline().split()))
+
+# for i in range(N):
+#     if X > my_list[i]:
+#         print(my_list[i],end=" ")
+
+#4-3
+
+# import sys
+# N = int(sys.stdin.readline())
+# my_list = list(map(int,sys.stdin.readline().split()))
+# print(min(my_list), max(my_list))
+
+#4-4
+import sys
+my_list = []
+for i in range(9):
+    N= int(sys.stdin.readline())
+    my_list.append(N)
+    
+print(max(my_list))
+print(my_list.index(max(my_list))+1)
+    
